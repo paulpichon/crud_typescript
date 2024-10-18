ea59c7793547ec1851d13e6edef053d934b140aa
paul pichon <paul10_barca@hotmail.com>
2024-10-17 18:29:33 -0600

    Create: creacion de usuario con interface

--- src/controllers/usuarios.ts
+++ src/controllers/usuarios.ts
@@ -1,15 +1,32 @@
+// imporatmos Usuario schema
+import Usuario from "../models/usuario";
+// Request, Response
+// Se debe importar request, reponse de @types/express
+//para que req.body sea reconocido
+import { request, response } from "express";
+// Interface de Usuario
+import { UsuarioInterface } from "../types/interfaces";
+
+
 // GET
-const usuariosGet = (req = Request, res = Response) => {
+const usuariosGet = (req = request, res = response) => {
     res.json({
       msg: 'Get - API'
     })
 }
 // POST
-const usuariosPost = async (req = Request, res = Response) => {
-
-    res.json({
-      msg: 'POST - API'
-    })
+const usuariosPost = async (req = request, res = response) => {
+    //Obtener los datos del body
+    // Utilizamos UsuarioInterface para decirle al objeto que debe tener esos types
+    // se usa para asegurarse de que un objeto en tiempo de ejecución cumpla con esa estructura.
+    const {nombre, correo, password, rol}: UsuarioInterface = req.body;
+  
+    // crear un usuario
+    const usuario = new Usuario<UsuarioInterface>({nombre, correo, password, rol});
+    // guardar en la BD
+    await usuario.save();
+    // respuesta
+    res.json(usuario)
 }
 // PUT
 const usuariosPut = (req = Request, res = Response) => {
