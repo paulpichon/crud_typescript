2d44b24cc6473099ebf1726f8e3e17e896236e79
paul pichon <paul10_barca@hotmail.com>
2024-10-22 16:29:19 -0600

    Update: defginicion de tipo Request, Response

--- src/controllers/usuarios.ts
+++ src/controllers/usuarios.ts
@@ -1,47 +1,41 @@
-// import bcryptjs
-import bcryptjs from "bcryptjs";
 // imporatmos Usuario schema
 import Usuario from "../models/usuario";
 // Request, Response
 // Se debe importar request, reponse de @types/express
 //para que req.body sea reconocido
-import { request, response } from "express";
+import { Request, Response } from "express";
 // Interface de Usuario
 import { UsuarioInterface } from "../types/interfaces";
 
 
 // GET
-const usuariosGet = (req = request, res = response) => {
+const usuariosGet = (req: Request, res: Response) => {
     res.json({
       msg: 'Get - API'
     })
 }
 // POST
-const usuariosPost = async (req = request, res = response) => {
+const usuariosPost = async (req: Request, res: Response) => {
     //Obtener los datos del body
     // Utilizamos UsuarioInterface para decirle al objeto que debe tener esos types
     // se usa para asegurarse de que un objeto en tiempo de ejecución cumpla con esa estructura.
     const {nombre, correo, password, rol}: UsuarioInterface = req.body;
+  
     // crear un usuario
     const usuario = new Usuario<UsuarioInterface>({nombre, correo, password, rol});
-    // TODO: encriptar la password
-    if (password) {
-      const salt = bcryptjs.genSaltSync(10);
-      usuario.password = bcryptjs.hashSync(password, salt);
-    }
     // guardar en la BD
     await usuario.save();
     // respuesta
     res.json(usuario)
 }
 // PUT
-const usuariosPut = (req = Request, res = Response) => {
+const usuariosPut = (req: Request, res: Response) => {
     res.json({
       msg: 'PUT - API'
     })
 }
 // DELETE
-const usuariosDelete = (req = Request, res = Response) => {
+const usuariosDelete = (req: Request, res: Response) => {
     res.json({
       msg: 'DELETE - API'
     })
