70ec74433168f0d8c4b51a68caaf2f911e6b901e
paul pichon <paul10_barca@hotmail.com>
2024-10-23 15:58:34 -0600

    Fix: encripta la contraseña de usuario

--- src/controllers/usuarios.ts
+++ src/controllers/usuarios.ts
@@ -1,3 +1,5 @@
+// bcryptjs
+import bcryptjs from 'bcryptjs';
 // imporatmos Usuario schema
 import Usuario from "../models/usuario";
 // Request, Response
@@ -20,13 +22,17 @@ const usuariosPost = async (req: Request, res: Response) => {
     // Utilizamos UsuarioInterface para decirle al objeto que debe tener esos types
     // se usa para asegurarse de que un objeto en tiempo de ejecución cumpla con esa estructura.
     const {nombre, correo, password, rol}: UsuarioInterface = req.body;
-  
     // crear un usuario
     const usuario = new Usuario<UsuarioInterface>({nombre, correo, password, rol});
+    // encriptar la contraseña
+    if ( password ) {
+      const salt = bcryptjs.genSaltSync(10);
+      usuario.password = bcryptjs.hashSync(password, salt);
+    }
     // guardar en la BD
     await usuario.save();
     // respuesta
-    res.json(usuario)
+    res.json(usuario);
 }
 // PUT
 const usuariosPut = (req: Request, res: Response) => {
