b419a6d914b98dd9a2a60a993b4d2a72f05551b3
paul pichon <paul10_barca@hotmail.com>
2024-10-28 16:32:05 -0600

    Refactor: se agrega anotacion de tipo de retorno

--- src/helpers/validar-correo-usuario.ts
+++ src/helpers/validar-correo-usuario.ts
@@ -4,7 +4,7 @@ import { UsuarioInterface } from "../types/interfaces"
 import Usuario from "../models/usuario"
 
 // funcion para validar le correo del usuario
-const validarCorreo = async ( correo = '' )  =>  {
+const validarCorreo = async ( correo: string = '' ): Promise<void> =>  {
     // verififcar si hay coincidencia de correos
     const existeCorreo = await Usuario.findOne({ correo });
     // si hay coincidencias moatramos los errores
