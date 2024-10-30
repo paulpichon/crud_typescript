0bc3472ca74ff944c29f359d0774e899b540fdc9
paul pichon <paul10_barca@hotmail.com>
2024-10-29 18:09:40 -0600

    feat: Se crea interface de Roles de usuario

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -8,7 +8,7 @@ interface UsuarioInterface {
     google?: boolean;
     img?: string;
     // En TypeScript, esto se denomina string union type.
-    // Significa que la propiedad "rol" sólo puede ser uno de los 2 string: 'pendiente' o 'pagado'.
+    // Significa que la propiedad "rol" sólo puede ser uno de los 2 string: 'ADMIN_ROL' o 'VENTAS_ROL'.
     rol: 'ADMIN_ROL' | 'VENTAS_ROL';
     estado?: boolean;
 }
