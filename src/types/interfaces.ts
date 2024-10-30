1967df658cd2d15ce94fffb6044111accce80516
paul pichon <paul10_barca@hotmail.com>
2024-10-30 16:28:01 -0600

    refactor: se agrega union type para rol

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -9,12 +9,12 @@ interface UsuarioInterface {
     img?: string;
     // En TypeScript, esto se denomina string union type.
     // Significa que la propiedad "rol" sólo puede ser uno de los 2 string: 'ADMIN_ROL' o 'VENTAS_ROL'.
-    rol: 'ADMIN_ROL' | 'VENTAS_ROL';
+    rol: 'SUPER_ADMIN_ROLE' | 'ADMIN_ROL' | 'VENTAS_ROL';
     estado?: boolean;
 }
 // Interface de Roles
 interface RolesUsuario {
-    rol: string;
+    rol: 'SUPER_ADMIN_ROLE' | 'ADMIN_ROL' | 'VENTAS_ROL';
 }
 
 // Exports
