e6626ba2ebb0ac4d6f705d090b02c85781368c76
paul pichon <paul10_barca@hotmail.com>
2024-10-31 17:08:25 -0600

    feat: se añade middlewares para ruta PUT

--- src/routes/usuarios.ts
+++ src/routes/usuarios.ts
@@ -23,9 +23,9 @@ router.get('/', usuariosGet);
 // POST
 router.post('/', [
     // Validar el nombre, no este vacio y sea string
-    check('nombre', 'El nombre es obligatorio').trim().notEmpty(),
+    check('nombre', 'El nombre es obligatorio').trim().notEmpty().escape(),
     // validar el correo
-    check('correo', 'El email no es valido').normalizeEmail().isEmail(),
+    check('correo', 'El email no es valido').trim().normalizeEmail().isEmail(),
     // que sea unico, que sea un correo valido
     check('correo').custom( validarCorreo ),
     // validar el password
@@ -33,13 +33,30 @@ router.post('/', [
     // validar el rol del usuario
     // check('rol', 'El rol no es valido: {ADMIN_ROL, VENTAS_ROL}').isIn(['ADMIN_ROL', 'VENTAS_ROL']),
     // verificar el ROL desde una ase e datos
-    check('rol').custom( esRolValido ),
-
+    check('rol').custom( esRolValido ).trim().notEmpty(),
     // Validar los campos
     validarCampos,
 ], usuariosPost);
 // PUT
-router.put('/:id', usuariosPut);
+router.put('/:id', [
+    // Validar el nombre, no este vacio y sea string
+    check('nombre', 'El nombre es obligatorio').optional().trim().notEmpty().escape(),
+    // Validar apellidos, no este vacio y sea string
+    check('apellidos', 'Los apellidos deben tene minimo: 2 caracteres').optional().trim().notEmpty().escape(),
+    // validar el password
+    check('password', 'El password es obligatorio: {min: 6 caracteres}').optional().trim().notEmpty().isLength({ min: 6 }),
+    // Validar img, no este vacio y sea string
+    check('img', 'La  img debe ser un string').optional().trim().notEmpty(),
+    // validar el rol del usuario
+    // check('rol', 'El rol no es valido: {ADMIN_ROL, VENTAS_ROL}').isIn(['ADMIN_ROL', 'VENTAS_ROL']),
+    // verificar el ROL desde una ase e datos
+    check('rol').custom( esRolValido ).optional().trim().notEmpty(),
+    // Validar estado
+    // true | false no pueden ser strings
+    check('estado', 'El estado debe ser true or false').optional().trim().notEmpty().isBoolean(),
+    // Validar los campos
+    validarCampos,
+], usuariosPut);
 // DELETE
 router.delete('/:id', usuariosDelete);
 
