27269ae7cdeb849eb2c1ba3c9ad656960bf97118
paul pichon <paul10_barca@hotmail.com>
2024-11-05 16:36:44 -0600

    Refactor: se pone anotacion de tipo de retorno(Return Type Annotations)

--- src/models/usuario.ts
+++ src/models/usuario.ts
@@ -42,7 +42,8 @@ const usuarioSchema = new Schema<UsuarioInterface>({
 usuarioSchema.methods.toJSON = function(): UsuarioInterface { 
     // Desestructuramos lo que no queremos que aparezca 
     const {password, __v, ...usuario} = this.toObject();
-    
+
+    // Para ver mas: https://chatgpt.com/c/672a9a2b-9408-8002-88e7-506cb5847136
     // Creamos un nuevo objeto para devolver y evitamos modificar el original
     // Object.assign es un método de JavaScript que se utiliza para copiar las propiedades de uno o más objetos de origen a un objeto destino. El objetivo principal de Object.assign es combinar objetos o hacer copias superficiales de ellos, útil para clonar objetos y evitar la mutación de los originales.
     const usuarioFiltrado: UsuarioInterface = Object.assign({}, usuario);
