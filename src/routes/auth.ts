db2e91e538456d8d809f4f6d89aeddb40e35b451
paul pichon <paul10_barca@hotmail.com>
2024-11-14 14:41:57 -0600

    feat: se añade middlewares para validar correo y password

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,11 +2,22 @@
 const { Router } = require("express");
 // renombrar la funcion
 const router = Router();
+// expree validator
+import { check } from "express-validator";
 // login
 import { login } from "../controllers/auth";
+// express validator
+import { validarCampos } from "../helpers/validar-campos";
 
 // POST
-router.post('/login', login);
+router.post('/login', [
+    // Validar correo
+    check('correo', 'El correo es obligatorio').trim().notEmpty().escape().isEmail(),
+    // validar password
+    check('password', 'El password es obligatorio').notEmpty(),
+    // Validar los campos
+    validarCampos,
+], login);
 
 // exports
 module.exports = router;
