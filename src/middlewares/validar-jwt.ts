6796a2fe48077a9d8eddc391ce0f550df6714253
paul pichon <paul10_barca@hotmail.com>
2024-11-25 16:09:45 -0600

    feat: se agregar validaciones y se crea la propiedad req.usuario para saber quien se ha logueado

--- src/middlewares/validar-jwt.ts
+++ src/middlewares/validar-jwt.ts
@@ -2,21 +2,23 @@
 import { NextFunction, Request, Response } from "express";
 // jsonwebtoken
 import jwt from 'jsonwebtoken';
+// Usuario modelo
+import Usuario from "../models/usuario";
 
 // validar el JWT para que puedan borrar registros
 const validarJWT = (req: Request, res: Response, next: NextFunction) => {
+    // obtener el token
+    const token = req.header('x-token') as string;  
+    // validar si hay token
+    if (!token) {
+        return res.status(404).json({
+            msg:'No hay token en la peticion'
+        });
+    }
     try {
-        // obtener el token
-        const token = req.header('x-token') as string;  
-        // validar si hay token
-        if (!token) {
-            return res.status(404).json({
-                msg:'No hay token en la peticion'
-            });
-        }
         // validamos el token
         const uuid = jwt.verify(token, process.env.PRIVATEORPUBLICKEY as string, 
-            function(err, decoded) {
+            async function(err, decoded) {
                 
                 // si el token expiro
                 if (err) {
@@ -25,13 +27,28 @@ const validarJWT = (req: Request, res: Response, next: NextFunction) => {
                     });
                 }
                 // desestructurar el decoded
-                const { uuid, exp } = decoded as jwt.JwtPayload;
-                console.log(uuid, 'USER ID');
-                console.log(exp, 'exp');
-                
-            });
-        // siguiente middleware
-        next();
+                const { uuid } = decoded as jwt.JwtPayload;
+                // validar que el UUID exista en la BD
+                const usuario = await Usuario.findById( uuid );
+                if (!usuario) {
+                    return res.status(404).json({
+                        msg: 'Token no valido - El usuario no existe en la BD'
+                    });    
+                }
+                // validar que el estado del usuario sea true
+                if ( !usuario.estado) {
+                    return res.status(404).json({
+                        msg: 'Token invalido - estatus:false'
+                    });
+                }
+                // creamos una propiedad nueva usuario en el request
+                // Se crea una propieda nueva en el Request.Express para poder usar req.usuario
+                // Explicacion del codigo de abajo: https://chatgpt.com/c/673e3cd1-cca8-8002-8e06-e1bd6968db4d
+                req.usuario = usuario;
+                // siguiente middleware
+                next();
+            }); 
+            
     } catch (error) {
         // console.log( error );
         return res.status(500).json({ 
