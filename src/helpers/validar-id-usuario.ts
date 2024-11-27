ebe1ba806324a0b9f16854ea3759ca7ae9abfc04
paul pichon <paul10_barca@hotmail.com>
2024-11-27 12:29:09 -0600

    feat: se agrego un if condition para validar que el usuario a eliminar tenga estado:true

--- src/helpers/validar-id-usuario.ts
+++ src/helpers/validar-id-usuario.ts
@@ -8,7 +8,11 @@ const existeIdUsuario = async ( id = ''): Promise<void> => {
     const usuarioIdExiste = await Usuario.findById( id );
     // si NO existe, mostramos una alerta
     if ( !usuarioIdExiste ) {
-        throw new Error(`El ID: ${id} no existe en la BD`);
+        throw new Error(`El ID: ${id} no existe en la BD - usuario no existe`);
+    }
+    // validar que el usuario tenga estado: true
+    if ( !usuarioIdExiste.estado ) {
+        throw new Error(`El ID: ${id} no existe en la BD - estado :false`);
     }
 }
 
